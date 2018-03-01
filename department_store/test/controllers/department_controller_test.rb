ab8b8f71142e510c93a515477c8f3e5654b33d90
Ashley Margallo <amarie921@yahoo.com>
2018-02-28 17:51:15 -0700

    initial commit

---
+++ department_store/test/controllers/department_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class DepartmentControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get department_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get department_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get department_edit_url
+    assert_response :success
+  end
+
+end
